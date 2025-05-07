33fa0bf2ac1e76b2e2ab8c17191259f1bc9cee16
neelasatyasai <neelasatyasai9.3@gmail.com>
2025-05-07 23:53:28 +0530

    added type text for input

--- X_displayname/src/App.jsx
+++ X_displayname/src/App.jsx
@@ -7,21 +7,48 @@ function App() {
   const [firstName, setfirstName] = useState("");
   const [lastName, setlastName] = useState("");
 
-function handleSubmit(event){
-  event.preventDefault();
-   setfirstName(event.target.elements.firstname.value);
-  setlastName(event.target.elements.lastname.value);
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    setfirstName(event.target.elements.firstname.value);
+    setlastName(event.target.elements.lastname.value);
+  }
 
   return (
     <>
       <h1>Full Name Display</h1>
       <form className="form" onSubmit={handleSubmit}>
-       <div> <label htmlFor="first">First Name :</label> <input id="first"name="firstname" required placeholder="enter first name"/></div> 
-       <div> <label htmlFor="last">Last Name :</label> <input id="last"name="lastname" required placeholder="enter last name"/></div> 
-       <button className="button" type="submit"> submit</button>
+        <div>
+          {" "}
+          <label htmlFor="first">First Name :</label>{" "}
+          <input
+            id="first"
+            name="firstname"
+            type="text"
+            required
+            placeholder="enter first name"
+          />
+        </div>
+        <div>
+          {" "}
+          <label htmlFor="last">Last Name :</label>{" "}
+          <input
+            id="last"
+            name="lastname"
+             type="text"
+            required
+            placeholder="enter last name"
+          />
+        </div>
+        <button className="button" type="submit">
+          {" "}
+          submit
+        </button>
       </form>
-      {firstName && lastName &&  ( <div className="displaytext"><span>Full Name:</span> <h3>{`${firstName} ${lastName}`}</h3> </div>) }
+      {firstName && lastName && (
+        <div className="displaytext">
+          <span>Full Name:</span> <h3>{`${firstName} ${lastName}`}</h3>{" "}
+        </div>
+      )}
     </>
   );
 }
